96bc2e014a9b7a22f9f4cd918ce55b231e7e176a
settingcons <settingcons@gmail.com>
2015-09-01 15:52:27 +0200

    20150901

--- www/js/context.js
+++ www/js/context.js
@@ -11,7 +11,8 @@ window.addEventListener("load", initMp3Player, false);
 
 function initMp3Player(){
 
-    context = new webkitAudioContext();
+    //context = new webkitAudioContext();
+    context = (window.AudioContext || window.webkitAudioContext)();
     analyser = context.createAnalyser();
 
     canvas = document.getElementById('analyser_render');
@@ -31,15 +32,21 @@ function deviceReady() {
     catch (ex){alert('deviceReady: '+ex.message);}
 }
 
+function Reproducir0(){
+    loadSound_2();
+}
+
 
 function Reproducir1(){
     var v_fichero = _mediaAudioFicheroIOS;
+    v_fichero = "audio/1ra-e.mp3";
     loadSound(v_fichero);
 }
 
 function Reproducir(){
     alert('Reproducir1');
     var v_fichero = ObtenerFicheroAudio();
+    v_fichero = "audio/testaudio.wav";
     loadSound(v_fichero);
 }
 
@@ -55,8 +62,48 @@ function bufferSoundiOS(event) {
     alert('3 bufferSoundiOS');
     mySource = source;
     alert('4 bufferSoundiOS');
-    source.noteOn(0);
+    source.start(0);
     alert('5 bufferSoundiOS');
+}
+
+function loadSound_2() {
+    try
+    {
+        // Create a instance of AudioContext interface
+        window.AudioContext = window.AudioContext||window.webkitAudioContext||window.mozAudioContext;
+        context = new AudioContext();
+
+        var source = context.createBufferSource(); //this represents the audio source. We need to now populate it with binary data.
+
+        //now retrieve some binary audio data from <audio>, ajax, input file or microphone and put it into a audio source object.
+        //here we will retrieve audio binary data via AJAX
+        var request = new XMLHttpRequest();
+        request.open('GET', 'audio/1ra-e.mp3', true);
+        request.responseType = 'arraybuffer'; //This asks the browser to populate the retrieved binary data in a array buffer
+        request.onload = function(){
+            //populate audio source from the retrieved binary data. This can be done using decodeAudioData function.
+            //first parameter of decodeAudioData needs to be array buffer type. So from wherever you retrieve binary data make sure you get in form of array buffer type.
+            context.decodeAudioData(request.response, function(buffer) {
+                source.buffer = buffer;
+            }, null);
+        }
+        request.send();
+
+        //now we got context, audio source.
+        //now lets connect the audio source to a destination(hardware to play sound).
+        source.connect(context.destination);//destination property is reference the default audio device
+
+        /*
+         If we wanted to add any audio nodes then we need to add them in between audio source and destionation anytime dynamically.
+         */
+
+        //now play the sound.
+        source.start(0);
+    }
+    catch(e)
+    {
+        alert("Web Audio API not supported");
+    }
 }
 
 function loadSound(url) {
