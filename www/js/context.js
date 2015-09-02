0b929485a5def48e244f37905e49d03820981543
settingcons <settingcons@gmail.com>
2015-09-02 15:33:46 +0200

    20150902_29

--- www/js/context.js
+++ www/js/context.js
@@ -458,7 +458,7 @@ function LeerFicheroAudioOKIOS(file){
 
 function TransformarFicheroAudioToBase64IOS(file) {
     file.type='audio/wav';
-    alert('fileSystem.root --> ' + fileSystem.root);
+    alert(' fileSystem.root --> ' + fileSystem.root);
     var reader = new FileReader();
     reader.onloadend = function(evt) {
         _inciAudioFichero = evt.target.result;
